2451423ce8a6c450ac06b7a3def77256d51e1f16
Kevin Blanco <kevinjblanco@gmail.com>
2022-11-01 17:11:01 -0400

    heroku deployment changes

--- server.js
+++ server.js
@@ -13,10 +13,14 @@ app.use(express.json())
 
 app.use(cors())
 app.use(logger('dev'))
+app.use(express.static(`${__dirname}/client/build`))
 // app.use() middleware here ^ ///////////////////
 
 app.use('/api', routes)
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+app.get('/*', (req, res) => {
+  res.sendFile(`${__dirname}/client/build/index.html`)
+})
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
