3a68216a264a59bbc24166ddca4d8f27c83f3c12
Kevin Blanco <kevinjblanco@gmail.com>
2022-11-01 09:34:52 -0400

    updated middleware

--- seed/kids.js
+++ seed/kids.js
@@ -13,7 +13,7 @@ const main = async () => {
       description:
         'The little Road Runners in your group will give ol’ Wile E. Coyote a run for his money on this curvy, wavy, junior roller coaster that will be absolutely hair-raising for young children.Rustle up your little ones, step into a classic Acme mining car and get ready for a sweet and breezy ride that will remind you of scenes from classic Road Runner cartoons. This will be a short but spirited zing through woodsy trails, suitable for kids over 33” with a parent and over 36” riding on their own.  The steepest hill is only 8 feet, and the top speed is 10 miles per hour, but you will get some hustle on the twists, turns, and mini-drops, sure to give your junior daredevils breathtaking thrills.',
       height: '36"',
-      capacity: '40',
+      capacity: '40 people',
       parkLocation: 'Bugs Bunny World',
       flashPass: 'available',
       year: '2005',
@@ -28,7 +28,7 @@ const main = async () => {
       description:
         'All aboard the tour bus for a wacky ride made just for your smallest ducklings to enjoy.  But when your tour guide is that crazy duck, you know you’re in for the ride of your life. Even the littlest tykes will love a seat on Daffy’s magical tour bus, which leaves the ground and gently floats low in the air as it circles its scenic route of colorful trees surrounding snow covered mountains, and a view of Bugs Bunny World!  That crazy Daffy sometimes even drives the bus backwards.  But the wide bench seats and nice slow pace make this the comfiest, safest, easiest tour in town.',
       height: '36"',
-      capacity: '20',
+      capacity: '20 people',
       parkLocation: 'Bugs Bunny World',
       height: '36in',
       flashPass: 'available',
@@ -44,7 +44,7 @@ const main = async () => {
       description:
         "'Silly wabbit, it's time to lift off in a hot air balloon with your host Elmer Fudd!  Climb into one of Elmer’s colorful striped balloon baskets and get ready to lift into the air. Now you're cruisin', as your personal hot air balloon circles, floating up and around.  You can simply look over the edge of your basket to enjoy a view of the Looney Tunes neighborhood on your mellow flight, or your little daredevils can grab hold of the center disk and give it a spin it to make the balloon spin wildly around.  Try it and you'll finally know what they mean by “dizzying heights.”  As Elmer would say, it's “vewy, vewy” fun!",
       height: '36"',
-      capacity: '42',
+      capacity: '42 people',
       parkLocation: 'Bugs Bunny World',
       flashPass: 'available',
       year: '2007',
@@ -59,7 +59,7 @@ const main = async () => {
       description:
         'Children can enjoy gentle hills and pint sized thrills on this junior-sized open-air roller coaster. Young thrill seekers will ride up a 10 foot tall hill and see the sights in the charming little train village of Whistlestop Park.',
       height: '36"',
-      capacity: '15',
+      capacity: '15 people',
       parkLocation: 'Whistlestop Park',
       flashPass: 'available',
       year: '2010',
@@ -73,7 +73,7 @@ const main = async () => {
       description:
         'Bugs Bunny World has its very own carousel, a mini country fairground replica even the smallest cowboys and cowgirls will love to ride.  Saddle up on the friendly horse of your choice - you can almost hear them neigh as they gently bob up and down in time to the music. Round and round you go, on a calmly-paced ride scaled down for little hands and feet.  Tykes get to have fun while they train for the day when they can take on the big rides, but this carousel is also very relaxing for their chaperones.',
       height: '36"',
-      capacity: '50',
+      capacity: '50 people',
       parkLocation: 'Bugs Bunny World',
       flashPass: 'available',
       year: '2007',
@@ -88,7 +88,7 @@ const main = async () => {
       description:
         "Speedy Gonzales Hot Rod Racers are part of Bugs Bunny World and is our fourth roller coaster — more than any other U.S. theme park! Kids and parents will hop aboard the miniature hot rod race cars together and enjoy an action-packed ride. You'll experience plenty of drops and thrills as you sprint around the grand prix track. This mini-coaster is the perfect training ground for future thrill seekers. One day they’ll ride Tatsu, Goliath and Full Throttle but in the meantime, here’s a fun and friendly smaller-scale roller coaster for the little ones in your crew.",
       height: '36"',
-      capacity: '12',
+      capacity: '12 people',
       parkLocation: 'Bugs Bunny World',
       flashPass: 'available',
       year: '2010',
@@ -103,7 +103,7 @@ const main = async () => {
       description:
         "Speedy Gonzales Hot Rod Racers are part of Bugs Bunny World and is our fourth roller coaster — more than any other U.S. theme park! Kids and parents will hop aboard the miniature hot rod race cars together and enjoy an action-packed ride. You'll experience plenty of drops and thrills as you sprint around the grand prix track. This mini-coaster is the perfect training ground for future thrill seekers. One day they’ll ride Tatsu, Goliath and Full Throttle but in the meantime, here’s a fun and friendly smaller-scale roller coaster for the little ones in your crew.",
       height: '36"',
-      capacity: '12',
+      capacity: '12 people',
       parkLocation: 'Bugs Bunny World',
       flashPass: 'available',
       year: '2010',
